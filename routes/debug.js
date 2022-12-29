2ee693fd5ca22ebdabdb9b9ce6b78736d8aac4ab
Muhammad Salmaan Alfarisi <amuhammadsalman45@gmail.com>
2022-12-29 11:58:30 +0700

    Integrating with cataas api

--- routes/debug.js
+++ routes/debug.js
@@ -1,23 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var bcrypt = require('bcrypt');
-var User = require('./model/userModel');
+const fetch = require('node-fetch');
 
+async function fetchCatId() {
+	const response = await fetch('https://cataas.com/api/cats?limit=5&skip=0');
+	const data = await response.json();
+	
+	let catId = data.map(cat => cat['_id']);
+	return catId;
+}
 
 /* For debug purpose only */
 router.get('/', async function (req, res, next) {
-  let key = 'password';
-  let user = await User.findAll({
-    attributes: ['username','password']
-  })
-  let pass = 'user2'
-  let user2 = user.find(user => {
-    return user['username'] == 'user2' && bcrypt.compareSync(pass, user['password']);
-  })
-  res.send({
-    dir: __dirname,
-    user: user2
-  });
+	let limit = 5
+	const response = await fetch('https://cataas.com/api/cats?limit=10&skip=0')
+	
+	console.log(await fetchCatId())
+	res.json({
+		"empty": "empty",
+		"data": fetchCatId()
+	})
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var fetch = require('node-fetch');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+async function fetchCatId() {
+  const response = await fetch('https://cataas.com/api/cats?limit=5&skip=0');
+  const data = await response.json();
+
+  let catId = await data.map(cat => cat['_id']);
+  return catId;
+}
+
+router.get('/', async function (req, res, next) {
   let authorized = req.session.authorized;
-  res.render('index', {authorized: authorized});
+  res.render('index', {authorized: authorized, catId: await fetchCatId()});
 });
 
 module.exports = router;
